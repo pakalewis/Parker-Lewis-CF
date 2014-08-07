627b4a931c1e8ba9bc29e66e4cb1c566993f82ee
Parker <pakalewis@gmail.com>
2014-08-06 20:08:42 -0700

    testing with a new project

---
+++ testing123/testing123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing123
+//
+//  Created by Parker Lewis on 8/6/14.
+//  Copyright (c) 2014 Parker Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
