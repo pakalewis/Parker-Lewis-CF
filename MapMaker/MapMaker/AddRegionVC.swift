ded9b3acbd9d5d8662403ae79e4a4c480c600de8
Parker <pakalewis@gmail.com>
2014-11-04 16:54:05 -0800

    Use notification center to start monitoring regions. Store Reminders with CoreData. Show regions on map with overlays.

--- MapMaker/MapMaker/AddRegionVC.swift
+++ MapMaker/MapMaker/AddRegionVC.swift
@@ -8,20 +8,21 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class NewRegionVC: UIViewController, UITextFieldDelegate {
 
-    var locationManager : CLLocationManager!
     var selectedAnnotation : MKAnnotation!
+    let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
 
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Delegates
         self.textField.delegate = self
         
         self.myLabel.text = "Latitude: \(self.selectedAnnotation.coordinate.latitude)\nLongitude: \(self.selectedAnnotation.coordinate.longitude)"
@@ -30,9 +31,6 @@ class NewRegionVC: UIViewController, UITextFieldDelegate {
     
     
     
-    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
-        return true
-    }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.textField.resignFirstResponder()
@@ -41,13 +39,46 @@ class NewRegionVC: UIViewController, UITextFieldDelegate {
     
     
     @IBAction func addRegionButton(sender: AnyObject) {
+        // Core Data
+        let managedObjectContext = appDelegate.managedObjectContext!
         
-        
-        println(self.textField.text)
-        var newRegion = CLCircularRegion(center: self.selectedAnnotation.coordinate, radius: 100.0, identifier: self.textField.text)
-        self.locationManager.startMonitoringForRegion(newRegion)
-        self.dismissViewControllerAnimated(true, completion: nil)
+        // First check to see if a Reminder with that identifier is already stored
+        var fetchRequest = NSFetchRequest(entityName: "Reminder")
+        fetchRequest.predicate = NSPredicate(format:"identifier = %@", self.textField.text)
+        var fetchResult = managedObjectContext.executeFetchRequest(fetchRequest, error: nil)
+        if fetchResult?.count > 0 {
+            var duplicateAlert = UIAlertController(title: "", message: "There is already a reminder with this name", preferredStyle: UIAlertControllerStyle.Alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
+            duplicateAlert.addAction(okAction)
+            self.presentViewController(duplicateAlert, animated: true, completion: nil)
+        }
+        else {
+            // Make new region to monitor
+            var newRegionToMonitor = CLCircularRegion(center: self.selectedAnnotation.coordinate, radius: 20000.0, identifier: self.textField.text)
+            println("new region called \(self.textField.text) added at \(newRegionToMonitor.center.latitude) and \(newRegionToMonitor.center.longitude)")
+            appDelegate.locationManager.startMonitoringForRegion(newRegionToMonitor)
+
+            // make new entity to store in core data
+            let newReminderToStore = NSEntityDescription.insertNewObjectForEntityForName("Reminder", inManagedObjectContext: managedObjectContext) as Reminder
+            newReminderToStore.identifier = self.textField.text
+            newReminderToStore.date = NSDate()
+            newReminderToStore.latitude = selectedAnnotation.coordinate.latitude
+            newReminderToStore.longitude = selectedAnnotation.coordinate.longitude
+            newReminderToStore.radius = newRegionToMonitor.radius
+            
+            var error : NSError?
+            managedObjectContext.save(&error)
+            if error != nil {
+                println("There was an error saving to core data. The error says \(error!.localizedDescription)")
+            }
+            
+            
+            // Post a notification
+            let newRegionNotification = NSNotification(name: "NEW_REGION_ADDED", object: nil, userInfo: ["region" : newRegionToMonitor])
+            NSNotificationCenter.defaultCenter().postNotification(newRegionNotification)
+
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
     }
-    
-    
 }
--- MapMaker/MapMaker/MapVC.swift
+++ MapMaker/MapMaker/MapVC.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var regions = appDelegate.locationManager.monitoredRegions
+        
+        
+        // Core Data stuff
+        let managedObjectContext = appDelegate.managedObjectContext!
+        
+        var fetchRequest = NSFetchRequest(entityName: "Reminder")
+        var sortDescriptor = NSSortDescriptor(key: "identifier", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        var fetchResult = managedObjectContext.executeFetchRequest(fetchRequest, error: nil)
+        
+        if fetchResult?.count > 0 {
+            println("there are \(fetchResult?.count) results stored in core data")
+            for result in fetchResult! {
+                let reminder = result as Reminder
+                println("Reminder named \(reminder.identifier) at \(reminder.makeCoordinate().latitude) and \(reminder.makeCoordinate().longitude)")
+            }
+        } else {
+            println("No Reminders stored yet")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +47,7 @@ class MapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         // Delegates
         appDelegate.locationManager.delegate = self
         self.mapView.delegate = self
+        
         
         // Get authorization to access location
         switch CLLocationManager.authorizationStatus() as CLAuthorizationStatus {
@@ -42,14 +70,46 @@ class MapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         // Set up gesture recognizer
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: "longPressOnMap:")
         self.mapView.addGestureRecognizer(longPressGesture)
+        
+        
+        // set up to observe when a region is created
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "newRegionAdded:", name: "NEW_REGION_ADDED", object: nil)
+        
+        
     }
     
     override func viewDidAppear(animated: Bool) {
-        var totalRegions = appDelegate.locationManager.monitoredRegions.count
-        var lastRegionAdded = appDelegate.locationManager.monitoredRegions.allObjects
-        println("Now monitoring \(totalRegions) total regions")
-        println("The latest region added was \(lastRegionAdded.last)")
+//        var totalRegions = appDelegate.locationManager.monitoredRegions.count
+//        var lastRegionAdded = appDelegate.locationManager.monitoredRegions.allObjects
+//        println("Now monitoring \(totalRegions) total regions")
+//        println("The latest region added was \(lastRegionAdded.last)")
     }
+    
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        println("YOU ARE ENTERING THE REGION \(region.identifier)")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        println("YOU ARE EXITING THE REGION \(region.identifier)")
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println("we got a location update!")
+        
+        if let location = locations.last as? CLLocation {
+            println(location.coordinate.latitude)
+            
+        }
+    }
+
+    
     
     func longPressOnMap(sender: UILongPressGestureRecognizer) {
         println("User long pressed")
@@ -66,6 +126,18 @@ class MapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     }
 
     
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        // Make renderer which draws the overlays on the map
+        let renderer = MKCircleRenderer(overlay: overlay)
+        
+        // Adjust settings on the renderer
+        renderer.fillColor = UIColor.greenColor().colorWithAlphaComponent(0.3)
+        
+        // Return it
+        return renderer
+    }
+    
+    
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
         let regionAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "REGION_ANNOTATION")
         regionAnnotation.animatesDrop = true
@@ -79,10 +151,18 @@ class MapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
  
     func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
         let newRegionVC = self.storyboard?.instantiateViewControllerWithIdentifier("NEW_REGION") as NewRegionVC
-        newRegionVC.locationManager = appDelegate.locationManager
         newRegionVC.selectedAnnotation = view.annotation
         self.presentViewController(newRegionVC, animated: true, completion: nil)
     }
  
+    
+    func newRegionAdded(notification : NSNotification) {
+        println("new region added and notification fired")
+        let userInfo = notification.userInfo!
+        let newRegion = userInfo["region"] as CLCircularRegion
+        
+        let overlay = MKCircle(centerCoordinate: newRegion.center, radius: newRegion.radius)
+        self.mapView.addOverlay(overlay)
+    }
     
 }
