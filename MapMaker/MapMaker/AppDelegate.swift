b05e12c96349b5efcc82ca8bcc9f0a7245cc0f68
Parker <pakalewis@gmail.com>
2014-11-05 14:20:58 -0800

    Enable iCloud. Add FetchedResultsController to populate tableview of Reminders

--- MapMaker/MapMaker/AppDelegate.swift
+++ MapMaker/MapMaker/AppDelegate.swift
@@ -67,7 +67,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("MapMaker.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
-        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+        
+        let options =
+            [ NSPersistentStoreUbiquitousContentNameKey: "CloudNotes",
+            NSMigratePersistentStoresAutomaticallyOption: true,
+            NSInferMappingModelAutomaticallyOption: true ]
+        
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options, error: &error) == nil {
             coordinator = nil
             // Report any error we got.
             let dict = NSMutableDictionary()
--- MapMaker/MapMaker/MonitoredRegionsVC.swift
+++ MapMaker/MapMaker/MonitoredRegionsVC.swift
@@ -8,33 +8,73 @@
 
 import UIKit
 import MapKit
+import CoreData
 
-
-class MonitoredRegionsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MonitoredRegionsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+    var managedObjectContext : NSManagedObjectContext!
+    var fetchedResultsController: NSFetchedResultsController!
+//    var monitoredRegions = [Reminder]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.dataSource = self
-        self.tableView.delegate = self
+//        self.tableView.delegate = self
+        
+        self.managedObjectContext = self.appDelegate.managedObjectContext!
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didGetCloudChanges:", name: NSPersistentStoreDidImportUbiquitousContentChangesNotification, object: appDelegate.persistentStoreCoordinator)
 
+        
+        // make a fetch request to core data to get all Reminders
+        var fetchRequest = NSFetchRequest(entityName: "Reminder")
+        var sortDescriptor = NSSortDescriptor(key: "identifier", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        // initialize FRC
+        self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext:self.managedObjectContext, sectionNameKeyPath: nil, cacheName: "Reminders")
+        self.fetchedResultsController.delegate = self
+        
+        var error : NSError?
+        if !self.fetchedResultsController.performFetch(&error) {
+            println("error!!")
+        }
     }
 
+    
+    func didGetCloudChanges(notification : NSNotification)
+    {
+        println("Did get cloud changes")
+        //self.managedObjectContext.performBlock { () -> Void in
+        self.managedObjectContext.mergeChangesFromContextDidSaveNotification(notification)
+        //}
+    }
+
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        println("reloading table view in controllerDidChangeContent")
+        self.tableView.reloadData()
+    }
+    
+    
+    // MARK: Tableview funcs
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return appDelegate.locationManager.monitoredRegions.count
+        var retval = self.fetchedResultsController.fetchedObjects?.count ?? 0
+        println("number of rows in section = \(retval)")
+        return retval
     }
  
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("REGION_CELL", forIndexPath: indexPath) as UITableViewCell
         
         // get appropriate region
-        let monitoredRegions = appDelegate.locationManager.monitoredRegions.allObjects
-        let currentRegion = monitoredRegions[indexPath.row] as CLRegion
+        let currentReminder = self.fetchedResultsController.fetchedObjects?[indexPath.row] as Reminder
         
-        cell.textLabel.text = currentRegion.identifier
+        cell.textLabel.text = currentReminder.identifier
         return cell
     }
             
