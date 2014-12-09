6224c1afdfe428226a89b3f4f5f6a91517bbdfc4
Parker <pakalewis@gmail.com>
2014-12-08 16:17:42 -0800

    Remove project that was moved to a new repo

--- StackOverFlowed/Pods/Target Support Files/Pods-StackOverFlowed-SVProgressHUD-0.8.1/Pods-StackOverFlowed-SVProgressHUD-0.8.1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StackOverFlowed_SVProgressHUD_0_8_1 : NSObject
-@end
-@implementation PodsDummy_Pods_StackOverFlowed_SVProgressHUD_0_8_1
-@end
